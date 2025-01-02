2d5e51b28fdc228f844ecdb899962583b2cbc22e
RamNikhil411 <nikhil.b@orotron.com>
2025-01-02 10:45:19 +0530

    8th commit

---
+++ assets/JS/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.deck',{
+    slidesPerView :1,
+    speed:1000,
+    mousewheel:true,
+    pagination:{
+        el:'.swiper-pagination',
+        type:'progressbar'
+    }
+})
